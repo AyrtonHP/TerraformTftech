0340d4827d8e18e4935bc7ed837a4c3a8e728d42
Ayrton Vitorino <74015097+AyrtonHP@users.noreply.github.com>
2025-02-27 20:30:16 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-provider "Azure" {
+provider "Azurerm" {
+    features {}
+    subscription_id = "849ef98a-c16c-4203-bee1-c9173474d038"
+    client_id = "d1fea6d7-baa6-48ea-b099-db1f817f57d9"
+    client_secret = "9Hl8Q~K5FIsF1S5U1c8AtxvmvLuOAMYU7h~EJcf4"
+    tenant_id = "b894caac-a195-478b-9d75-a36335cf667b"
     
-  
 }
