5792064d1ac0bbe51a6b623531fa7208203e3608
Ayrton Vitorino <74015097+AyrtonHP@users.noreply.github.com>
2025-02-27 21:09:59 -0300

    ok

--- MOD01_LAB01/MOD01_LAB01/provider.tf
+++ MOD01_LAB01/MOD01_LAB01/provider.tf
@@ -1,8 +1,7 @@
-provider "Azurerm" {
-    features {}
+provider "azurerm" {
+  features {}
     subscription_id = "849ef98a-c16c-4203-bee1-c9173474d038"
     client_id = "d1fea6d7-baa6-48ea-b099-db1f817f57d9"
     client_secret = "9Hl8Q~K5FIsF1S5U1c8AtxvmvLuOAMYU7h~EJcf4"
     tenant_id = "b894caac-a195-478b-9d75-a36335cf667b"
-    
 }
